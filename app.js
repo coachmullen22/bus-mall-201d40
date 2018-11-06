fc178fbf6620dc720a0c3bad52d79c340e5b2e34
Jeff <coachmullen@gmail.com>
2018-11-05 17:19:40 -0800

    I can't find whatever is causing the infinite loop in my code. It's been blocking me since I got TA help. I'll keep working on this but the local storage and table do render if you console log it. displayPics() breaks it now. It didn't before.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function Product(name) {
   var products = ['bag', 'banana', 'boots','breakfast', 'bubblegum', 'bathroom', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 }
 
-/*
+
 window.onload = function(){
   if(localStorage.getItem('cumulativeTotal')) {
     var retrieveItems = JSON.parse(localStorage.getItem('cumulativeTotal'));
@@ -40,7 +40,6 @@ window.onload = function(){
     });
   }
 }
-*/
 
 function makeRandom() {
   return Math.floor(Math.random() * allProducts.length);
